8ce5e16bd7ecef35b00389f7d1a6a4cba9786d42
Mihas Dainiak <mihanius@gmail.com>
2022-03-31 20:59:51 +0300

    f(x) method created

--- src/com/belhard/branching/Task2.java
+++ src/com/belhard/branching/Task2.java
@@ -4,15 +4,17 @@ import com.belhard.utils.ConsoleUtil;
 
 public class Task2 {
 
-	public static void main(String[] args) {
-		System.out.println("Equation: \nx * x - 3 * x + 9, when x <= 3 and\n1 / (x * x * x + 6), when x > 3.");
+    public static void main(String[] args) {
+        System.out.println("Equation: \nx * x - 3 * x + 9, when x <= 3 and\n1 / (x * x * x + 6), when x > 3.");
 
-		double x = ConsoleUtil.getDouble("x", 0);
+        double x = ConsoleUtil.getDouble("x", 0);
+        double result = f(x);
 
-		double result = (x <= 3) ? x * x - 3 * x + 9 : 1 / (x * x * x + 6);
+        System.out.printf("Equation result is %.6g%n", result);
+    }
 
-		System.out.println(String.format("Equation result is %.4f", result));
-
-	}
+    static double f(double x) {
+        return (x <= 3) ? x * x - 3 * x + 9 : 1 / (x * x * x + 6);
+    }
 
 }
