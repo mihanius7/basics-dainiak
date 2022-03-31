f5b0163a3712d3aa782d97233d3f34528d6b9cf6
Mihas Dainiak <mihanius@gmail.com>
2022-03-31 19:52:57 +0300

    ASCII table console output optimized

--- src/com/belhard/cycles/Task4.java
+++ src/com/belhard/cycles/Task4.java
@@ -2,11 +2,13 @@ package com.belhard.cycles;
 
 public class Task4 {
 
-	public static void main(String[] args) {
-		char ch;
-		for (int i = 33; i <= 128; i++) {
-			ch = (char) i;
-			System.out.println(i + " -> '" + ch + "'");
-		}
-	}
+    public static void main(String[] args) {
+        char ch;
+        for (int i = 33; i <= 128; i++) {
+            ch = (char) i;
+            System.out.printf("%3d -> %-2s\t", i, ch);
+            if (i % 8 == 0)
+                System.out.println();
+        }
+    }
 }
