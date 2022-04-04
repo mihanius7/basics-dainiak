c34c387fe0b15b270da775bc07d43da42825333f
Mihas Dainiak <mihanius@gmail.com>
2022-04-04 13:33:19 +0300

    Method 'getReversed' extracted

--- src/com/belhard/linear/Task2.java
+++ src/com/belhard/linear/Task2.java
@@ -4,15 +4,20 @@ import com.belhard.utils.ConsoleUtil;
 
 public class Task2 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		double input = ConsoleUtil.getDouble("nnn.ddd", 777.444);
+        double input = ConsoleUtil.getDouble("nnn.ddd", 777.444);
 
-		int integerPart = (int) input;
-		int decimalPart = (int) Math.round((input - integerPart) * 1000);
-		double reversedInput = Double.parseDouble(decimalPart + "." + integerPart);
+        double reversedInput = getReversedInput(input);
 
-		System.out.println("Result is " + reversedInput);
+        System.out.println("Result is " + reversedInput);
 
-	}
+    }
+
+    private static double getReversedInput(double input) {
+        int integerPart = (int) input;
+        int decimalPart = (int) Math.round((input - integerPart) * 1000);
+        double reversedInput = Double.parseDouble(decimalPart + "." + integerPart);
+        return reversedInput;
+    }
 }
