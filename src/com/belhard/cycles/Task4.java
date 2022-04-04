e0a097a470eb977942e3c41de86d4c38f7176c34
Mihas Dainiak <mihanius@gmail.com>
2022-04-04 13:34:19 +0300

    'Magic numbers' extracted in constants

--- src/com/belhard/cycles/Task4.java
+++ src/com/belhard/cycles/Task4.java
@@ -1,12 +1,14 @@
 package com.belhard.cycles;
 
 public class Task4 {
+    public static final int FIRST_PRINTABLE_CHAR = 33;
+    public static final int LAST_PRINTABLE_CHAR = 128;
 
     public static void main(String[] args) {
         char ch;
-        for (int i = 33; i <= 128; i++) {
+        for (int i = FIRST_PRINTABLE_CHAR; i <= LAST_PRINTABLE_CHAR; i++) {
             ch = (char) i;
-            System.out.printf("%3d -> %-2s\t", i, ch);
+            System.out.printf("%03d -> %-2s\t", i, ch);
             if (i % 8 == 0)
                 System.out.println();
         }
